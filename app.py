29b322bbff41db902804a32a050224f6831780b0
Sulaimon Olanrewaju <sirtunx.hokyky@gmail.com>
2023-04-19 06:33:54 +0100

    First commit

---
+++ app.py
@@ -0,0 +1,21 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///example.db'
+db = SQLAlchemy(app)
+
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(50))
+    email = db.Column(db.String(50), unique=True)
+
+
+@app.route('/')
+def index():
+    return 'Welcome to EUDAIMONIA web app. Let\'s get to building'
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
