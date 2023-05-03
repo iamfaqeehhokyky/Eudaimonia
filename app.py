c24e2a9ce81b2bb0154c5668bae647fa449953f7
Sulaimon Olanrewaju <sirtunx.hokyky@gmail.com>
2023-05-03 12:33:54 +0100

    Updated the server to add blog page route

--- app.py
+++ app.py
@@ -1,30 +1,70 @@
-from flask import Flask, render_template, request, g, session, jsonify
-from flask_sqlalchemy import SQLAlchemy
+from flask import Flask, render_template, request, g, session, jsonify, url_for, redirect
+from models import db
+from flask_migrate import Migrate
 import sqlite3
+import hashlib
+import os
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///example.db'
-db = SQLAlchemy(app)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///users.db'
+app.secret_key = os.urandom(16)
 
-# for mySQLite
+# initialize the app with the extension
+db.init_app(app)
+migrate = Migrate(app, db)
+
+#  for mySQLite
 app.secret_key = 'your'
 DATABASE_FILE = 'database.db'
 
 
-class User(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(50))
-    email = db.Column(db.String(50), unique=True)
+# Function handling the hash password
+def hash_password(password):
+    salt = os.urandom(16)
+    password_hash = hashlib.pbkdf2_hmac(
+        'sha256', password.encode('utf-8'), salt, 100000)
+    return salt + password_hash
+
+# Function handling the password checker for correctness and tallying
+
+
+def check_password(password, password_hash):
+    salt = password_hash[:16]
+    stored_password_hash = password_hash[16:]
+    new_password_hash = hashlib.pbkdf2_hmac(
+        'sha256', password.encode('utf-8'), salt, 100000)
+    return new_password_hash == stored_password_hash
+
+
+def get_user(user_id):
+    conn = sqlite3.connect('users.db')
+    c = conn.cursor()
+    c.execute("SELECT id, email, first_name, last_name, username, gender, university_name, password_hash FROM users WHERE id = ?", (user_id,))
+    row = c.fetchone()
+    conn.close()
+    return {'id': row[0], 'email': row[1], 'first_name': row[2], 'last_name': row[3], 'username': row[4], 'gender': row[5], 'university_name': row[6], 'password': row[7]}
+
+
+@app.before_request
+def load_user():
+    user_id = session.get('user_id')
+    if user_id is not None:
+        g.user = get_user(user_id)
+    else:
+        g.user = None
 
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    return render_template('index.html', user=g.user)
+
+# Route to the  blog page
 
 
 @app.route('/blog')
 def blog():
     return render_template('blog.html')
+
 
 # Usuage based on mySQLite
 
@@ -84,9 +124,11 @@ def replicate_table(table_name, new_table_name):
 
         # Insert the data from the original table into the new table
         db_execute("INSERT INTO %s SELECT * FROM %s" %
+
                    (new_table_name, table_name))
 
         print("Successfully replicated %s as %s" %
+
               (table_name, new_table_name))
     except sqlite3.Error as e:
         if str(e).startswith('table %s already exists' % new_table_name):
@@ -262,6 +304,81 @@ def grocery():
     for row in groceries:
         data.append({'Date': row[0], 'Item': row[1]})
     return jsonify(data)
+
+
+@app.route('/signup', methods=['GET', 'POST'])
+def signup():
+    if request.method == 'POST':
+        # Get form data
+        email = request.form['email']
+        first_name = request.form['first_name']
+        last_name = request.form['last_name']
+        username = request.form['username']
+        gender = request.form['gender']
+        university_name = request.form['university_name']
+        password = request.form['password']
+
+        # Hash password
+        password_hash = hash_password(password)
+
+        # Insert user into database
+        conn = sqlite3.connect('users.db')
+        c = conn.cursor()
+        c.execute("INSERT INTO users (email, first_name, last_name, username, gender, university_name, password_hash) VALUES (?, ?, ?, ?, ?, ?, ?)",
+                  (email, first_name, last_name, username, gender, university_name, password_hash))
+        conn.commit()
+        conn.close()
+
+        # Redirect to sign-in page
+        return redirect(url_for('signin'))
+
+    # Render sign-up page
+    return render_template('signup.html')
+
+
+@app.route('/signin', methods=['GET', 'POST'])
+def signin():
+    if request.method == 'POST':
+        # Get form data
+        email = request.form['email']
+        password = request.form['password']
+
+        # Check if user exists in database
+        conn = sqlite3.connect('users.db')
+        c = conn.cursor()
+        c.execute("SELECT id, password_hash FROM users WHERE email = ?", (email,))
+        row = c.fetchone()
+
+        if row is None:
+            # User not found
+            error = 'Invalid email or password'
+            return render_template('signin.html', error=error)
+
+        # Check password
+        password_hash = row[1]
+        if check_password(password, password_hash):
+            # Password is correct, store user ID in session
+            session['user_id'] = row[0]
+            return redirect('/')
+        else:
+            # Password is incorrect
+            error = 'Invalid email or password'
+            return render_template('signin.html', error=error)
+
+    # Render sign-in page
+    return render_template('signin.html')
+
+
+@app.route('/signout')
+def signout():
+    session.pop('user_id', None)
+    return redirect(url_for('index'))
+
+
+@app.route('/logout')
+def logout():
+    session.clear()
+    return redirect(url_for('signin'))
 
 
 if __name__ == '__main__':
