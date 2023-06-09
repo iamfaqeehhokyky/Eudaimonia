517f0b32bcb8b50c955ab3e8aa49ef78eb85e307
Enoch Epekipolu <epekipoluenoch@gmail.com>
2023-06-09 12:39:23 +0100

    added style to mental health

--- static/assets/js/mental.js
+++ static/assets/js/mental.js
@@ -1,25 +1,40 @@
-var apiKey = '{{ api_key }}';  // Retrieve the API key from Flask route
+// Loads the YouTube IFrame Player API asynchronously
+function loadYouTubeAPI() {
+  var tag = document.createElement('script');
+  tag.src = 'https://www.youtube.com/iframe_api';
+  var firstScriptTag = document.getElementsByTagName('script')[0];
+  firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+}
 
-// Load the YouTube IFrame Player API asynchronously
-var tag = document.createElement('script');
-tag.src = "https://www.youtube.com/iframe_api";
-var firstScriptTag = document.getElementsByTagName('script')[0];
-firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
-
-// Create an <iframe> (video player) for each video ID
-var players = [];
-function onYouTubeIframeAPIReady() {
-  {% for video_id in video_ids %}
-  players[{{ loop.index0 }}] = new YT.Player('player{{ loop.index }}', {
-    height: '360',
-    width: '640',
-    videoId: '{{ video_id }}',
+// Created an <iframe> (video player) for each video ID
+function createPlayer(videoId) {
+  new YT.Player(videoId, {
+    height: '100%',
+    width: '100%',
+    videoId: videoId,
     playerVars: {
-      modestbranding: 1,
-      showinfo: 0,
+      autoplay: 1,
       controls: 1,
-      key: apiKey  // Pass the API key to the playerVars
+      modestbranding: 1,
+      rel: 0
+    },
+    events: {
+      'onReady': onPlayerReady
     }
   });
-  {% endfor %}
 }
+
+// The API calls this function when the player is ready
+function onPlayerReady(event) {
+  // Do something when the player is ready
+}
+
+// Added click-to-play functionality to the video containers
+const videoContainers = document.querySelectorAll('.click-to-play');
+videoContainers.forEach((container) => {
+  container.addEventListener('click', () => {
+    const videoId = container.getAttribute('id');
+    container.style.display = 'none';
+    createPlayer(videoId);
+  });
+});
